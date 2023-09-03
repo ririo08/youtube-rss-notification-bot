af4a992f05d632e1303355fb2b8d6748d8e1dd13
ririo08 <ririo08@users.noreply.github.com>
2023-09-03 17:51:31 +0900

    fix: 2000字制限対応

--- src/sendDiscord.ts
+++ src/sendDiscord.ts
@@ -4,29 +4,42 @@ import { DiscordWebhook } from '../types/DiscordWebhook';
 import { RssFormat } from '../types/RssFormat';
 
 export const sendDiscord = async (parsedRss: RssFormat[]) => {
-  const content: string[] = [];
+  let content: string[] = [];
+  const requestContents: string[] = [];
+
   for (const user of parsedRss) {
     content.push('## :red_circle: ' + user.userName);
     for (const movie of user.movie) {
       content.push(movie.title + '\r' + movie.link + '\r');
     }
+    if (content.join('\r').length >= 1500) {
+      requestContents.push(content.join('\r'));
+      content = [];
+    }
   }
 
-  const req: DiscordWebhook = {
-    username: '動画フィード',
-    content: content.join('\r'),
-    embeds: [
-      {
-        title: format(subDays(new Date(), 1), 'M月d日') + 'の更新',
-      },
-    ],
-  };
+  if (requestContents.length === 0) {
+    requestContents.push(content.join('\r'));
+  }
 
-  await fetch(DISCORD_WEBHOOK_URL, {
-    method: 'POST',
-    body: JSON.stringify(req),
-    headers: { 'Content-Type': 'application/json' },
-  }).catch((e) => {
-    console.error('チャット送信', e);
-  });
+  let i = 0;
+  for (const requestContent of requestContents) {
+    const req: DiscordWebhook = {
+      username: format(subDays(new Date(), 1), 'M月d日') + 'の更新 #' + ++i,
+      content: requestContent,
+      embeds: [
+        {
+          title: '.',
+        },
+      ],
+    };
+
+    await fetch(DISCORD_WEBHOOK_URL, {
+      method: 'POST',
+      body: JSON.stringify(req),
+      headers: { 'Content-Type': 'application/json' },
+    }).catch((e) => {
+      console.error('チャット送信', e);
+    });
+  }
 };
